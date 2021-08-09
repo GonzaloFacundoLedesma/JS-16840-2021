bc30d02420996edd1a3d68ce918ee53b26f8c977
FranciscoPugh <franciscopugh01@gmail.com>
2021-08-09 12:50:09 -0300

    Primer Clase

---
+++ 1-Conceptos-Generales/main.js
@@ -0,0 +1,11 @@
+let numero1 = Number(prompt("Ingresa un numero"))
+
+let numero2 = Number(prompt("Ingresa otro numero para que se sumen"))
+
+alert("El resultado de la suma es " + (numero1 + numero2));
+
+// Este es un comentario
+
+/*
+    Este es otro comentario
+*/
